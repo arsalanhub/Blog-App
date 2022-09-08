2c873e36fc2534e7811cbefa7fbcd72e864b2181
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-08 20:20:06 +0530

    .

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.DB_CONNECTION);
+mongoose.connect("mongodb+srv://mohdarsalan:1234@cluster0.bpk8vbq.mongodb.net/?retryWrites=true&w=majority");
 const database = mongoose.connection;
 
 database.on('error', (error) => {
